5571db033150a8ffb101a0de0d5432c6a08cfcb0
ranqingwen <ranqingwen@gmail.com>
2026-04-07 19:08:19 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -17,6 +17,7 @@ function Diy_Part1() {
 }
 
 function Diy_Part2() {
+	# 统一改为 AutoUpdate
 	export UPDATE_TAG="AutoUpdate-${TARGET_BOARD}"
 	export FILESETC_UPDATE="${HOME_PATH}/package/base-files/files/etc/openwrt_update"
 	export GITHUB_PROXY="https://ghfast.top"
@@ -27,19 +28,6 @@ function Diy_Part2() {
 		echo -e "\n\033[0;31m缺少autoupdate/replace文件\033[0m"
 		exit 1
 	fi
-
-	# --- 核心修复：直接在函数内提取内核版本，不再依赖外部变量 ---
-	# 尝试从 include/kernel-version.mk 提取 (6.12.80 这种格式)
-	local KERN_V=$(grep -oP '(?<=LINUX_VERSION-6.12 = ).*' "${HOME_PATH}/include/kernel-version.mk" | tr -d ' ')
-	
-	# 2. 如果上面没匹配到，尝试通用的 LINUX_VERSION 提取
-	[ -z "$KERN_V" ] && KERN_V=$(grep "^LINUX_VERSION:=" "${HOME_PATH}/include/kernel-version.mk" | cut -d= -f2 | tr -d ' ')
-	
-	# 3. 如果还是没有，尝试从 Makefile 提取
-	[ -z "$KERN_V" ] && KERN_V=$(grep "KERNEL_PATCHVER:=" "${HOME_PATH}/target/linux/${TARGET_BOARD}/Makefile" | cut -d= -f2 | tr -d ' ')
-
-	# 赋值，确保不为空
-	export LINUX_KERNEL="${KERN_V:-6.12}"
 
 	# 识别设备型号
 	if [[ "${TARGET_PROFILE}" == *"k3"* ]]; then
@@ -59,24 +47,29 @@ function Diy_Part2() {
 	case "${TARGET_BOARD}" in
 	x86)
 		export FIRMWARE_SUFFIX=".img.gz"
-		# 格式：源码-版本-内核-型号-时间戳
-		export AUTOBUILD_FIRMWARE_UEFI="${SOURCE}-${LUCI_EDITION}-${LINUX_KERNEL}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
-		export AUTOBUILD_FIRMWARE="${SOURCE}-${LUCI_EDITION}-${LINUX_KERNEL}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
+		# 格式：版本-源码-型号-时间戳
+		export AUTOBUILD_FIRMWARE_UEFI="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
+		export AUTOBUILD_FIRMWARE="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
+	;;
+	rockchip | bcm27xx | mxs | sunxi | zynq |loongarch64 |omap |sifiveu |tegra |amlogic |mvebu)
+		export FIRMWARE_SUFFIX=".img.gz"
+		export AUTOBUILD_FIRMWARE="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 	;;
 	*)
 		export FIRMWARE_SUFFIX=".bin"
-		export AUTOBUILD_FIRMWARE="${SOURCE}-${LUCI_EDITION}-${LINUX_KERNEL}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
+		export AUTOBUILD_FIRMWARE="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 	;;
 	esac
 	
 	export FIRMWARE_VERSION="${SOURCE}-${TARGET_PROFILE_ER}-${UPGRADE_DATE}"
 
+	# 引导类型
 	if [[ "${TARGET_BOARD}" == "x86" ]]; then
-		BOOT_TYPE="bios"
+		BOOT_TYPE="legacy"
 		echo "AUTOBUILD_FIRMWARE_UEFI=${AUTOBUILD_FIRMWARE_UEFI}-uefi" >> ${GITHUB_ENV}
 		echo "AUTOBUILD_FIRMWARE=${AUTOBUILD_FIRMWARE}-${BOOT_TYPE}" >> ${GITHUB_ENV}
 	elif [[ "${FIRMWARE_SUFFIX}" == ".img.gz" ]]; then
-		BOOT_TYPE="bios"
+		BOOT_TYPE="legacy"
 		echo "AUTOBUILD_FIRMWARE=${AUTOBUILD_FIRMWARE}-${BOOT_TYPE}" >> ${GITHUB_ENV}
 	else
 		BOOT_TYPE="sysupgrade"
@@ -89,7 +82,7 @@ function Diy_Part2() {
 	echo "FIRMWARE_VERSION=${FIRMWARE_VERSION}" >> ${GITHUB_ENV}
 	echo "GITHUB_RELEASE=${GITHUB_RELEASE}" >> ${GITHUB_ENV}
 
-	# 写入openwrt_update文件
+	# 写入配置
 	install -m 0755 /dev/null "${FILESETC_UPDATE}"
 	echo "GITHUB_LINK=\"${GITHUB_LINK}\"" >> ${FILESETC_UPDATE}
 	echo "FIRMWARE_VERSION=\"${FIRMWARE_VERSION}\"" >> ${FILESETC_UPDATE}
@@ -102,12 +95,12 @@ function Diy_Part2() {
 	echo "RELEASE_DOWNLOAD=\"${RELEASE_DOWNLOAD}\"" >> ${FILESETC_UPDATE}
 	cat "$LINSHI_COMMON/autoupdate/replace" >> ${FILESETC_UPDATE}
 
-	# 写入del_assets文件
+	# 写入清理文件
 	install -m 0755 /dev/null "${GITHUB_WORKSPACE}/del_assets"
 	echo "UPDATE_TAG=\"${UPDATE_TAG}\"" >> "${GITHUB_WORKSPACE}/del_assets"
 	echo "BOOT_TYPE=\"${BOOT_TYPE}\"" >> "${GITHUB_WORKSPACE}/del_assets"
 	echo "FIRMWARE_SUFFIX=\"${FIRMWARE_SUFFIX}\"" >> "${GITHUB_WORKSPACE}/del_assets"
-	echo "FIRMWARE_PROFILEER=\"${SOURCE}-${LUCI_EDITION}-${TARGET_PROFILE_ER}\"" >> "${GITHUB_WORKSPACE}/del_assets"
+	echo "FIRMWARE_PROFILEER=\"${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}\"" >> "${GITHUB_WORKSPACE}/del_assets"
 }
 
 function Diy_Part3() {
@@ -116,40 +109,32 @@ function Diy_Part3() {
 	[[ ! -d "${BIN_PATH}" ]] && mkdir -p "${BIN_PATH}" || rm -rf "${BIN_PATH}"/*
 	
 	cd "${FIRMWARE_PATH}"
-	# 如果有 .img 但没有 .img.gz，则进行压缩
 	if [[ -n "$(ls -1 | grep -Eo '.img')" ]] && [[ -z "$(ls -1 | grep -Eo '.img.gz')" ]]; then
 		gzip -f9n *.img
 	fi
 	
 	case "${TARGET_BOARD}" in
 	x86)
-		# 处理 UEFI 固件
 		if [[ -n "$(ls -1 | grep -E 'efi')" ]]; then
 			EFI_ZHONGZHUAN="$(ls -1 |grep -Eo ".*squashfs.*efi.*img.gz" |grep -v ".vm\|.vb\|.vh\|.qco\|ext4\|root\|factory\|kernel")"
 			if [[ -f "${EFI_ZHONGZHUAN}" ]]; then
-				# 移除原有的 EFIMD5 变量及相关拼接，直接使用 Part2 定义的名称
-				cp -Rf "${EFI_ZHONGZHUAN}" "${BIN_PATH}/${AUTOBUILD_FIRMWARE_UEFI}${FIRMWARE_SUFFIX}"
+				# 重新计算并拼接 MD5
+				EFIMD5="$(md5sum ${EFI_ZHONGZHUAN} |cut -c1-3)$(sha256sum ${EFI_ZHONGZHUAN} |cut -c1-3)"
+				cp -Rf "${EFI_ZHONGZHUAN}" "${BIN_PATH}/${AUTOBUILD_FIRMWARE_UEFI}-${EFIMD5}${FIRMWARE_SUFFIX}"
 				echo "BOOT_UEFI=\"uefi\"" >> "${GITHUB_WORKSPACE}/del_assets"
-			else
-				echo "没找到在线升级可用的efi${FIRMWARE_SUFFIX}格式固件"
 			fi
 		fi
 		
-		# 处理 BIOS (原 legacy) 固件
 		if [[ -n "$(ls -1 | grep -E 'squashfs')" ]]; then
 			UP_ZHONGZHUAN="$(ls -1 |grep -Eo ".*squashfs.*img.gz" |grep -v ".vm\|.vb\|.vh\|.qco\|efi\|ext4\|root\|factory\|kernel")"
 			if [[ -f "${UP_ZHONGZHUAN}" ]]; then
-				# 移除原有的 MD5 变量及相关拼接
-				cp -Rf "${UP_ZHONGZHUAN}" "${BIN_PATH}/${AUTOBUILD_FIRMWARE}${FIRMWARE_SUFFIX}"
-			else
-				echo "没找到在线升级可用的${FIRMWARE_SUFFIX}格式固件"
+				MD5="$(md5sum ${UP_ZHONGZHUAN} | cut -c1-3)$(sha256sum ${UP_ZHONGZHUAN} | cut -c1-3)"
+				cp -Rf "${UP_ZHONGZHUAN}" "${BIN_PATH}/${AUTOBUILD_FIRMWARE}-${MD5}${FIRMWARE_SUFFIX}"
 			fi
-		else
-			echo "没有squashfs格式固件"
 		fi
 	;;
 	*)
-		# 其他机型的处理逻辑
+		# 其他机型匹配逻辑
 		if [[ -n "$(ls -1 | grep -E 'sysupgrade')" ]]; then
 			UP_ZHONGZHUAN="$(ls -1 |grep -Eo ".*${TARGET_PROFILE}.*sysupgrade.*${FIRMWARE_SUFFIX}" |grep -v ".vm\|.vb\|.vh\|.qco\|efi\|ext4\|root\|factory\|kernel")"
 		elif [[ -n "$(ls -1 | grep -E 'squashfs')" ]]; then
@@ -158,18 +143,15 @@ function Diy_Part3() {
 			UP_ZHONGZHUAN="$(ls -1 |grep -Eo ".*${TARGET_PROFILE}.*combined.*${FIRMWARE_SUFFIX}" |grep -v ".vm\|.vb\|.vh\|.qco\|efi\|ext4\|root\|factory\|kernel")"
 		elif [[ -n "$(ls -1 | grep -E 'sdcard')" ]]; then
 			UP_ZHONGZHUAN="$(ls -1 |grep -Eo ".*${TARGET_PROFILE}.*sdcard.*${FIRMWARE_SUFFIX}" |grep -v ".vm\|.vb\|.vh\|.qco\|efi\|ext4\|root\|factory\|kernel")"
-		else
-			echo "没找到在线升级可用的${FIRMWARE_SUFFIX}格式固件，或者没适配该机型"
 		fi
 
 		if [[ -f "${UP_ZHONGZHUAN}" ]]; then
-			# 统一移除哈希值拼接，直接复制
-			cp -Rf "${UP_ZHONGZHUAN}" "${BIN_PATH}/${AUTOBUILD_FIRMWARE}${FIRMWARE_SUFFIX}"
+			MD5="$(md5sum ${UP_ZHONGZHUAN} | cut -c1-3)$(sha256sum ${UP_ZHONGZHUAN} | cut -c1-3)"
+			cp -Rf "${UP_ZHONGZHUAN}" "${BIN_PATH}/${AUTOBUILD_FIRMWARE}-${MD5}${FIRMWARE_SUFFIX}"
 		fi
 	;;
 	esac
-
-	echo -e "\n\033[0;32m远程更新固件\033[0m"
+	echo -e "\n\033[0;32m远程更新固件准备就绪\033[0m"
 	ls -1 $BIN_PATH
 	cd ${HOME_PATH}
 }
